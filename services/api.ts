0d8cca939a287f8bbccdcdcef1f14e3b4cead517
Yumna Arif <arifyumna241@gmail.com>
2026-05-12 16:24:23 +0500

    update

--- services/api.ts
+++ services/api.ts
@@ -8,8 +8,8 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?api_key=${TMDB_CONFIG.API_KEY}&query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?api_key=${TMDB_CONFIG.API_KEY}&sort_by=popularity.desc`;
 
   const response = await fetch(endpoint, {
     method: "GET",
